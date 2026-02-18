8ef7d1118bed9fa9dfc982d310c90df9d5124ade
Himanshu <bohimanshu@gmail.com>
2026-02-19 00:25:18 +0530

    Android & ios permissions added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:background_location/app.dart';
+import 'package:background_location/ui/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: GuardianRouteApp(),
+      home: HomeScreen(),
     );
   }
 }
